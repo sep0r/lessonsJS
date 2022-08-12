0a30f0fa6b64b5f0bcac248ec7ecd61a8c09cc86
Dem <example@gmail.com>
2022-08-12 09:56:22 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* alert("hello"); */
+
+/* const result = confirm("Are you here?"); */
+/* 
+const answer = prompt("Вам есть 18?", "18");
+
+console.log(typeof(answer)); */
+
+const answer = [];
+
+/* answer[0] = prompt('Как ваше имя?', '');
+answer[1] = prompt('Какая ваша фамилия?', '');
+answer[2] = prompt('Сколько вам лет?', ''); */
+
+const name = prompt("Как вас зовут?", "");
+alert(`Добрый день, ${name} !`);
