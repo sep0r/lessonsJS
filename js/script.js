4dda1885fc3595727a9abb666835ba1fb0b2fc3b
Dem <example@gmail.com>
2022-08-26 16:30:00 +0500

    Task 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,9 +9,29 @@ const personalMovieDB = {
 };
 
 
-const lastFilm = prompt("Один из просмотренных фильмов?", "");
-const rating = prompt("На сколько оцените его", "");
+let lastFilm;
+let rating;
 
-personalMovieDB.movies[lastFilm] = rating;
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    lastFilm = prompt("Один из просмотренных фильмов?", "");
+    rating = prompt("На сколько оцените его", "");
+
+
+    if ((lastFilm && rating !== null) && ((lastFilm && rating) !== "") && (lastFilm.length < 50 && rating.length < 50)) {
+        personalMovieDB.movies[lastFilm] = rating;
+        console.log(personalMovieDB);
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотренно довольно мало фильмов!");
+} else if (personalMovieDB.count < 30 && personalMovieDB.count > 10) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман!");
+}
